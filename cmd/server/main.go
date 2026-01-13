12f9816e982dd5d062173fa9063772cd5b598d19
bntrtm <git.bntrtm@tjbainter.com>
2026-01-13 13:16:40 -0600

    feat: add server start

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+	ConnString := "amqp://guest:guest@localhost:5672/"
+	cxn, err := amqp.Dial(ConnString)
+	defer cxn.Close()
+	if err == nil {
+		fmt.Println("Connection to server successful!")
+	}
+
+	// server shutdown
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	fmt.Println("\nPeril server shutting down...")
 }
